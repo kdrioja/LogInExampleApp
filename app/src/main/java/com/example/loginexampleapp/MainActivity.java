31afd3cbde515a19f90c73298768628fb6f311ac
Kenia Rioja <kdrioja@gmail.com>
2019-08-14 15:49:18 -0700

    Added a Toast.makeText(context, text, Toast.LENGTH_...).show()

--- app/src/main/java/com/example/loginexampleapp/MainActivity.java
+++ app/src/main/java/com/example/loginexampleapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Info", "Button pressed.");
         Log.i("Username", usernameEditText.getText().toString());
         Log.i("Password", passwordEditText.getText().toString());
+
+        Toast.makeText(this, "Hi there!", Toast.LENGTH_LONG).show();
     }
 }
